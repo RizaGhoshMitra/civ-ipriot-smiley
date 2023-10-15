0b103f2f6a536f062974d962f6b775a9ba1f0d37
rizaghoshmitra <20111569@tafe.wa.edu.au>
2023-10-15 21:25:06 +0800

    adding smiley project

---
+++ sad.py
@@ -0,0 +1,40 @@
+from smiley import Smiley
+from blinkable import Blinkable
+import time
+class Sad(Smiley, Blinkable):
+    def __init__(self):
+        #super().__init__()
+        super().__init__(complexion=self.BLUE)
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def draw_mouth(self):
+        """
+        Method that draws the mouth on the standard faceless smiley.
+        """
+        mouth = [49, 54, 42, 43, 44, 45]
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+        """
+        Method that draws the eyes (open or closed) on the standard smiley.
+        :param wide_open: True if eyes opened, False otherwise
+        """
+        eyes = [10, 13, 18, 21]
+        for pixel in eyes:
+            self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
+
+    def blink(self, delay=0.25):
+        """
+        Make the happy smiley blink once with a certain delay (in s).
+        This is the implementation of the abstract method from the
+        Blinkable abstract class.
+
+        :param delay: Delay in seconds
+        """
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
---
+++ smiley.py
@@ -0,0 +1,43 @@
+from sense_hat import SenseHat
+
+
+class Smiley:
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    YELLOW = (255, 255, 0)
+    BLANK = (0, 0, 0)
+    BLUE = (0,0,255)
+
+    def __init__(self,complexion=YELLOW):
+        # We have encapsulated the SenseHat object
+        self.sense_hat = SenseHat()
+        self.my_complexion=complexion
+        Y = complexion
+        O = self.BLANK
+        self.pixels = [
+            O, Y, Y, Y, Y, Y, Y, O,
+            Y, Y, Y, Y, Y, Y, Y, Y,
+            Y, Y, Y, Y, Y, Y, Y, Y,
+            Y, Y, Y, Y, Y, Y, Y, Y,
+            Y, Y, Y, Y, Y, Y, Y, Y,
+            Y, Y, Y, Y, Y, Y, Y, Y,
+            Y, Y, Y, Y, Y, Y, Y, Y,
+            O, Y, Y, Y, Y, Y, Y, O,
+        ]
+
+    def complexion(self):
+        return self.my_complexion
+
+    def dim_display(self, dimmed=True):
+        """
+        Set the SenseHat's light intensity to low (True) or high (False)
+        :param dimmed: Dim the display if True, otherwise don't dim
+        """
+        self.sense_hat.low_light = dimmed
+
+    def show(self):
+        """
+        Show the smiley on the screen.
+        """
+        self.sense_hat.set_pixels(self.pixels)
